462c2bd100af378d41cf83d65d822d72a2d1e95e
JohnChewning <johnathanchewning@gmail.com>
2024-04-08 16:36:36 -0400

    added thought schema

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('./express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const express = require('express');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
